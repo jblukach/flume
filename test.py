1220f72502ff3873b7fed25dd4ca6cb333b9bc0d
John Lukach <hello@lukach.io>
2025-03-21 01:03:19 +0000

    datetime

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import base64
 import json
 import requests
 
@@ -12,20 +11,9 @@ data['two'] = 'MISSISSIPPI'
 data['three'] = 'MISSISSIPPI'
 
 r = requests.post(
-    'https://tf9tx7jk2b.execute-api.us-east-1.amazonaws.com//logs?verify=<VERIFY>',
+    'https://wcthbj9pld.execute-api.us-east-1.amazonaws.com/logs?verify=<VERIFY>',
     headers = headers,
     data = data
-)
-
-print(r.status_code)
-print(json.dumps(r.json(), indent=4))
-
-encoded = base64.b64encode(str(data).encode('utf-8')).decode('utf-8')
-
-r = requests.post(
-    'https://tf9tx7jk2b.execute-api.us-east-1.amazonaws.com//logs?verify=<VERIFY>',
-    headers = headers,
-    data = encoded
 )
 
 print(r.status_code)
